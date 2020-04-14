54c44de5866372e9d6bec9256b057eb25990c074
azdoolittle <drewdoolittle@gmail.com>
2020-04-14 13:34:27 -0400

    Wrote letter using var concat

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        String country = "Japan";
+        String capital = "Tokyo";
+        String currency = "Yen";
+        int dollarToYenConversionRate = 107;
+        int population = 126;
+
+        System.out.println("Hey Mom and Dad!");
+        System.out.println("Here are some interesting details about the first country I'm visiting.");
+        System.out.println("Name: " + country);
+        System.out.println("Population Size: " + population + " million");
+        System.out.println("Capital: " + capital);
+        System.out.println("Currency: One US Dollar equals " + dollarToYenConversionRate + " " + currency + "!!");
+        System.out.println("I'm so excited to visit the " + capital + " National Park and feel the giraffes!!");
+        System.out.println("Talk later.");
+    }
+}
